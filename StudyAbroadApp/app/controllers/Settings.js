366257e39cafa1ffdf38a8b6ebaa0b50e2ff93c3
GreenKnight15 <jamesritter15@gmail.com>
2015-04-28 13:06:07 -0500

    testing push

--- StudyAbroadApp/app/controllers/Settings.js
+++ StudyAbroadApp/app/controllers/Settings.js
@@ -4,3 +4,9 @@ function openMenu() {
     var index = Alloy.createController("index").getView();
 	index.open();
    } 
+   
+ 
+function OpenPush() {
+	var Settings = Alloy.createController("Puch Notifications").getView();
+	PushNotifications.open();
+}
