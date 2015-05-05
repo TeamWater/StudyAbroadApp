ae745f1835b3bc8e550fb1aebf4a0a695a088aac
GreenKnight15 <jamesritter15@gmail.com>
2015-05-05 14:38:33 -0500

    push form for ios

--- app/controllers/Push.js
+++ app/controllers/Push.js
@@ -1,28 +1,51 @@
 //James Ritter
+function openMenu() {
+    var index = Alloy.createController("index").getView();
+	index.open();
+   } 
+
+
 function makeNote() {
 	
-alert($.soundSwitch.value);
  $.soundSwitch.addEventListener('change',function(e){
-   $.soundSwitch.value = true;
-   alert($.basicSwitch.value);
+ 	if($.soundSwitch.value = true){
+   $.soundSwitch.value = false;
+  } else{
+  	$.soundSwitch.value = true;
+  }
+});
+
+ $.vibSwitch.addEventListener('change',function(e){
+ 	if($.vibSwitch.value = true){
+   $.vibSwitch.value = false;
+  } else{
+  	$.vibSwitch.value = true;
+  }
 });
 
 	var sound = $.soundSwitch.value;
-    var text = $.pushText.getValue();
+	var vib = $.vibSwitch.value;
+    var text = String($.pushText.getValue());
+    if(text == ""){
+    	alert('Please add to text field');
+    } else {
     Cloud.PushNotifications.notify({
         channel: 'all',
         payload: {
             "alert": text,
             "sound": sound,
-            "vibrate": false
+            "vibrate": vib,
         },
         to_ids: 'everyone'
     }, function(e) {
         if (e.success) {
             alert('Success');
+            openMenu();
+            $.pushText.value = null;
         } else {
             alert('Error:\n' +
                 ((e.error && e.message) || JSON.stringify(e)));
         }
     });
+  }
 }
