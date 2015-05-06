5380c1f20c690f30e5f3a3a31ca6837b0a6550dc
GreenKnight15 <jamesritter15@gmail.com>
2015-05-05 21:50:18 -0500

    small fixes

--- app/alloy.js
+++ app/alloy.js
@@ -8,12 +8,13 @@
 // accessible globally by attaching them to the `Alloy.Globals`
 // object. For example:
 //
-// Alloy.Globals.someGlobalFunction = function(){};
+
 Alloy.Globals.Map = require('ti.map');
 
 Alloy.Globals.FB = require('facebook');
 
 
+ 	
 Ti.Geolocation.purpose = 'Track Users Phone for map display';
 Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_HIGH ;
 Ti.Geolocation.distanceFilter = 10;
@@ -25,3 +26,4 @@ Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
   	Alloy.Globals.lat = e.coords.latitude;
   	Alloy.Globals.lng = e.coords.longitude;
   });
+      
