832e79e27387abb62a06cdb9e3913318066d6514
umhughes <umhughes@yahoo.com>
2015-05-05 19:20:31 -0500

    Made adjustments to have checklist display like agenda for uniformity

--- app/controllers/CheckList.js
+++ app/controllers/CheckList.js
@@ -1,18 +1,19 @@
-// Mark and Hues
-var args = arguments[0] || {};
+
+//Mark, Ussarian, Christian
+var Events = Alloy.Collections.instance("Events");
 
 function openMenu() {
     var index = Alloy.createController("index").getView();
-	index.open();
-   } 
-   
+    index.open();
+}
+
 var Cloud = require("ti.cloud");
 Cloud.debug = true;
 
-if(OS_ANDROID) {
-	
-var plainTemplate = {
-    childTemplates: [{
+if (OS_ANDROID) {
+
+    var plainTemplate = {
+        childTemplates: [{
             type: "Ti.UI.Label",
             bindId: "box",
             properties: {
@@ -20,147 +21,219 @@ var plainTemplate = {
                 width: Ti.UI.FILL,
                 top: "10dp",
                 bottom: "10dp",
-                left: "10dp",
+                left: "75dp",
                 right: "10dp",
+                borderRadius: "5dp",
+            }
+        }, {
+            type: "Ti.UI.Label",
+            bindId: "arrow",
+            properties: {
+                backgroundColor: "#e9e9e9",
+                width: "10dp",
+                height: "10dp",
+                transform: Ti.UI.create2DMatrix({
+                    rotate: 45
+                }),
+                top: "28dp",
+                left: "70dp",
+            }
+        }, {
+
+            type: "Ti.UI.Label",
+            bindId: "dot",
+            properties: {
+                backgroundColor: "#1da587",
+                width: "10dp",
+                height: "10dp",
+                top: "28dp",
+                left: "52dp",
                 borderRadius: "5dp",
             }
         }, {
             type: "Ti.UI.Label",
             bindId: "title",
             properties: {
-            	height: Ti.UI.FILL,
-            	width: Ti.UI.FILL,
+                height: Ti.UI.FILL,
+                width: Ti.UI.FILL,
                 color: "#424242",
                 font: {
                     fontFamily: "Arial",
                     fontSize: "21dp"
                 },
-                left: "65dp",
+                left: "120dp",
                 top: "20dp"
             },
         }, {
             type: "Ti.UI.Label",
             bindId: "details",
             properties: {
-            	height: Ti.UI.FILL,
-            	width: Ti.UI.FILL,
+                height: Ti.UI.FILL,
+                width: Ti.UI.FILL,
                 color: "#424242",
                 font: {
                     fontSize: "14dp"
                 },
-                left: "65dp",
+                left: "100dp",
                 top: "50dp",
                 bottom: "50dp",
-                
+
             },
-        },  
-        
-        {
-    	type: "Ti.UI.Switch",
-    	bindId: "check",
-    	properties: {
-    		borderColor: "#e9e9e9",
-    		style : Titanium.UI.Android.SWITCH_STYLE_CHECKBOX ,
-            left: "25dp",
-            top: "45dp",
-    	}	
-    },
-    ]};
-}
+        }, {
+            type: "Ti.UI.Label",
+            bindId: "date",
+            properties: {
+                height: "50dp",
+                left: "6dp",
+                width: "60dp",
+                top: "5dp",
+                right: "200dp",
+                color: "#e9e9e9",
+
+            }
+        }, ]
+    };
 
 
-if(OS_IOS) {
-	
-var plainTemplate = {
-    childTemplates: [{
-        type: "Ti.UI.Label",
-        bindId: "box",
-        properties: {
-            backgroundColor: "white",
-            width: Ti.UI.FILL,
-            height: Ti.UI.SIZE,
+    var listView = Ti.UI.createListView({
+        templates: {
+            uncheck: plainTemplate,
         },
-    }, 
-    
-    {
-        type: "Ti.UI.Label",
-        bindId: "title",
-        properties: {
-            color: "#424242",
-            width: Ti.UI.SIZE,
-            height: Ti.UI.SIZE,
-            left: "30dp",
-            top: "5dp"
-        },
-    },
-    
-     {
-        type: "Ti.UI.Label",
-        bindId: "details",
-        properties: {
-            color: "gray",
-            height: Ti.UI.SIZE,
-            width: Ti.UI.FILL,
-            left: "30dp",
-            top: "25dp",
-        },
-    }, 
-    
-     {
-    	type: "Ti.UI.Switch",
-    	bindId: "check",
-    	properties: {
-            left: "5dp",
-            top: "5dp",
-    	}
-    	
-    },
-    ]};
-} 
+        defaultItemTemplate: "uncheck",
+        separatorColor: "transparent",
+    });
 
-var listView = Ti.UI.createListView({
-    templates: {
-        uncheck: plainTemplate
-    },
-    separatorColor: "transparent",
-    defaultItemTemplate: "uncheck"
-});
-var section = Ti.UI.createListSection();
- 	listView.sections = [ section ];
+    var section = Ti.UI.createListSection();
+    listView.sections = [section];
 
-dateView = Ti.UI.createListView();
+    var postdata = [];
+    var sectionViews = [];
 
-var data = [];
-var checkList = ['5537ec78657a500a270e8e99'];
+Cloud.Posts.query({
+                order: "create_date",
+            }, function(e) {
+        for (var i = 0; i < e.posts.length; i++) {
+                    var post = e.posts[i];
+                    var moment = require('alloy/moment');
+                    var day = moment(post.start_time, "YYYY-MM-DD:HH:mm:ssZZ");
+                    var fixedDate = day.format("MM-DD");
 
-for (var i = 0; i < checkList.length; i++) {
-Cloud.Posts.show({post_id: checkList[i] },function (e) {
-	if (e.success) {
-        var post = e.posts[0];
-        	data.push({
-        	
-            	box : {},
-            	title: { text: post.title},
-            	details: { text: post.content},
-            	check: {}
+                    postdata.push({
+
+                        box: {},
+                        arrow: {},
+
+                        title: {
+                            text: post.title
+                        },
+                        details: {
+                            text: post.content
+                        },
+                        date: {
+                            text: fixedDate,
+                        },
+
+                    });
+
+                    section.setItems(postdata);
+                }
+               }
+            ); 
+
+    var line = Ti.UI.createView();
+        var dateView = Ti.UI.createView(); 
+        sectionView = Ti.UI.createView(); 
+        sectionView.add(listView); 
+        dateView.add(sectionView); 
+        $.win.add(dateView); $.win.open();
+    }
+
+
+    if (OS_IOS) { 	
+        var tableData = [];
+            Cloud.Posts.query({
+                order: "create_date",
+            }, function(e) {
+                if (e.success) {
+        for (var i = 0; i < e.posts.length; i++) {
+                    var post = e.posts[i];
+                    var moment = require('alloy/moment');
+                    var day = moment(post.start_time, "YYYY-MM-DD:HH:mm:ssZZ");
+                    var fixedDate = day.format("MM-DD");
             
-            });       
-                section.setItems(data);
+                    row = Ti.UI.createTableViewRow({
+                        className: 'forumEvent',
+                        selectedBackgroundColor: 'white',
+                        rowIndex: i, 
+                        
+                    });
+
+                    var box = Ti.UI.createLabel({
+                        backgroundColor: "#e9e9e9",
+                        width: Ti.UI.FILL,
+                        top: 10,
+                        
+                        left: 10,
+                        right: 10,
+                        borderRadius: 5,
+
+                    });
+                    row.add(box);
+
+                    var title = Ti.UI.createLabel({
+                        text:post.title,
+                        width: Ti.UI.FILL,
+                        color: "#424242",
+                        font: {
+                            fontFamily: "Arial",
+                            fontSize: 21
+                        },
+                        left: 70,
+                        top: 3,
+
+                    });
+                    row.add(title);
+
+                    var details = Ti.UI.createLabel({
+                        text: post.content,
+                        
+                        width: Ti.UI.FILL,
+                        color: "#424242",
+                        font: {
+                            fontSize: 14
+                        },
+                        left: 70,
+                        top:30,
+                        horizontalWrap:true,
+
+                    });
+                    row.add(details);
+
+                    var date = Ti.UI.createLabel({
+                        text: fixedDate,
+                        height: 50,
+                        left: 6,
+                        width: 60,
+                        top: 5,
+                        right: 200,
+                        color: "424242",
+
+                    });
+                    row.add(date);
+                    
+                  tableData.push(row);     
+                }
+                
+                 var tableView = Ti.UI.createTableView({
+            backgroundColor: '#e9e9e9',
+            data: tableData,
+            headerTitle:"Agenda",
+        });
+
+        $.win.add(tableView);
+        $.win.open();
             };
-  	});
-    checkList[i] = Ti.UI.createView();
-    checkList[i].add(listView);
-};
-
-var scrollableView = Ti.UI.createScrollableView({
-    views: checkList,
-    showPagingControl: true,
-    horizontalWrap: false
-});
-
-var dateView = Ti.UI.createView();
-sectionView = Ti.UI.createView();
-sectionView.add(scrollableView);
-dateView.add(sectionView);
-$.win.add(dateView);
-$.win.open();
+           }
+        );
+       
+    }
